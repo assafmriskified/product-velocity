77ea99a5d507c960234596bb1072c7f0f213cc5c
assaf.miller <assaf.miller@riskified.com>
2022-12-01 14:34:17 +0200

    changes

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -8,51 +8,51 @@ import logo from "../../image.png";
 
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    padding: 40px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: 40px;
 `;
 
 const TopNav = styled.div`
-    display: flex;
-    margin: 0 0 20px 0;
+  display: flex;
+  margin: 0 0 20px 0;
 `;
 
 const MainContainer = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
 interface DashboardProps {
-    productName: string,
-    id: number,
+  productName: string,
+  id: number,
 }
 const initialState : DashboardProps = {
-    productName: 'Nintendo Switch',
-    id: 0,
+  productName: 'Nintendo Switch',
+  id: 0,
 }
 
 
 const Dashboard = (): JSX.Element => {
-    const [value, setValue] = useState<DashboardProps>(initialState)
-    return (
-        <Container>
-            <TopNav>
-              <div>
-                <img src={logo} alt="Logo" width={"30%"}/>
-              </div>
-              <div>
-                <DateFilter></DateFilter>
-              </div>
-            </TopNav>
-            <MainContainer>
-                <ProductScorecard product={value.productName} merchantName={'Riskistore'}></ProductScorecard>
-                <ProductList value={value} setValue={setValue}></ProductList>
-                <CountChart productId={value.id} loading={false} noData='none' isEmpty={false}/>
-                <InventoryCharts productId={value.id} loading={false} noData='none' isEmpty={false}/>
-            </MainContainer>
-        </Container>
-    );
+  const [value, setValue] = useState<DashboardProps>(initialState)
+  return (
+    <Container>
+      <div>
+        <img src={logo} alt="Logo" width={"30%"}/>
+      </div>
+      <TopNav>
+        <div>
+          <DateFilter></DateFilter>
+        </div>
+      </TopNav>
+      <MainContainer>
+        <ProductScorecard product={value.productName} merchantName={'Riskistore'}></ProductScorecard>
+        <ProductList value={value} setValue={setValue}></ProductList>
+        <CountChart productId={value.id} loading={false} noData='none' isEmpty={false}/>
+        <InventoryCharts productId={value.id} loading={false} noData='none' isEmpty={false}/>
+      </MainContainer>
+    </Container>
+  );
 };
 
 export default Dashboard;
